ee86a3ef2b868ce4667c1f8af09668637cdccd05
ryosuke kuwabara <ryosuke0711993@gmail.com>
2019-08-10 15:34:50 +0900

    add data processing file

---
+++ data.py
@@ -0,0 +1,61 @@
+import glob
+from tensorflow.core.example import example_pb2
+import struct
+from nltk.tokenize import word_tokenize, sent_tokenize
+from utils import *
+from tqdm import tqdm
+
+
+PATH="/home/ryosuke/desktop/data_set/cnn_stories_tokenized/"
+
+
+def sent_split(text):
+    words =[sent for sent in sent_tokenize(text)]
+    words = list(map(lambda x:x.split(),words))
+    return [word for inner_list in words for word in inner_list]
+
+
+
+def data_generate(vocab, num_of_data):
+    file = open(PATH+"train.bin","rb")
+    # file = open(PATH+"val.bin","rb")
+    #valでやって見る
+
+    articles = []
+    abstracts = []
+    print('# of data', num_of_data)
+
+    i=0
+    pbar = tqdm(total=num_of_data)
+    while i<num_of_data:
+        len_bytes = file.read(8)
+        if not len_bytes:
+            print('finishied reading this files')
+            break
+
+        #連続するバイト列から単位ごとに区切る
+        str_len = struct.unpack('q',len_bytes)[0]
+        example_str = struct.unpack('%ds' % str_len, file.read(str_len))[0]
+
+        #区切られた単位からarticle/abstractを取り出す
+        data = example_pb2.Example.FromString(example_str)
+        article = data.features.feature["article"].bytes_list.value[0]
+        if len(article)<=0:
+          continue
+        abstract = data.features.feature["abstract"].bytes_list.value[0]
+
+        #バイトコードを文字列に変換する（articleの場合はセンテンスとの一つずつに<s><\s>を入れている）
+        article = sent_split(article.decode())[:MAX_INPUT_LENGTH]
+        abstract = " ".join(vocab.abstract2sents(abstract.decode())).split()[:MAX_OUTPUT_LENGTH]
+
+        #IDに変更している
+        article = [vocab._word2id(word) for word in article][:MAX_INPUT_LENGTH]
+        abstract = [START_DECODING_NO]+[vocab._word2id(word) for word in abstract]+[STOP_DECODING_NO]
+        articles.append(article)
+        abstracts.append(abstract)
+        i+=1
+        pbar.update(1)
+    print('data successfully　constructed!')
+    print()
+
+    return articles, abstracts
